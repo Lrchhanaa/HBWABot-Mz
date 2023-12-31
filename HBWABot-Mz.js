ef8b25f5c63070dc677c4de850efdb9b259ee37e
Herbert-Suantak <149988596+Herbert-Suantak@users.noreply.github.com>
2023-12-31 15:24:27 +0530

    Add files via upload

--- HBWABot-Mz.js
+++ HBWABot-Mz.js
@@ -42,9 +42,9 @@ const acr = new acrcloud({
   access_secret: "U0PEUg2y6yGVh6NwJra2fJkiE1R5sCfiT6COLXuk",
 });
 const prem1 = JSON.parse(fs.readFileSync('./asset/database/premium.json'))
-const const elnglo = 'https://raw.githubusercontent.com/HBMods-OFC/Base/main/HBWABot-v3/Premium.json';
-const englo2 = await fetch(englo);
-const prem3 = await englo2.json();
+const elnglo = 'https://raw.githubusercontent.com/HBMods-OFC/Base/main/HBWABot-v3/Premium.json';
+const englo2 = fetch(englo);
+const prem3 = englo2.json();
 const prem = JSON.parse(prem3);
 const hbmods = JSON.parse(fs.readFileSync('./asset/Base/Developer.json'))
 const Englo = JSON.parse(fs.readFileSync('./asset/Base/Ka-hming.json'))
