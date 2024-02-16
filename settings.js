429399fd019502a8b01d9858f5c92541e40e6006
Herbert-Suantak <149988596+Herbert-Suantak@users.noreply.github.com>
2024-02-16 12:05:58 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -9,7 +9,7 @@ global.autoRecord = false // true a i dah chuan Voice record angin i lang ang
 global.available = false // false a i dah chuan offline angin a awm ang
 global.autoread = false // true a i dah chuan message zawng zawng read vek zelin a awm ang 
 global.autoread_status = false // true a i dah chuan auto status view in a awm ang
-global.bot_language = [`ja`]
+global.bot_language = ['hi']
 //info
 global.owner = ['918416093656'] // I phone number in thlak rawh
 global.ownernumber = '918416093656' // I phone number in thlak rawh
