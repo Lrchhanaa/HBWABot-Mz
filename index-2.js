1ae4c1989cdadf940aee5adef79a4684898147cf
Herbert-Suantak <149988596+Herbert-Suantak@users.noreply.github.com>
2024-03-08 11:19:53 +0530

    Add files via upload

--- index-2.js
+++ index-2.js
@@ -52,31 +52,6 @@ HBWABotMz.ev.on('connection.update', async (update) => {
 		lastDisconnect
 	} = update
 try{
-		if (connection === 'close') {
-			let reason = new Boom(lastDisconnect?.error)?.output.statusCode
-			if (reason === DisconnectReason.badSession) {
-				console.log(`Bad Session File, Please Delete Session and Scan Again`);
-				startHBWABotMz()
-			} else if (reason === DisconnectReason.connectionClosed) {
-				console.log("Connection closed, reconnecting....");
-				startHBWABotMz();
-			} else if (reason === DisconnectReason.connectionLost) {
-				console.log("Connection Lost from Server, reconnecting...");
-				startHBWABotMz();
-			} else if (reason === DisconnectReason.connectionReplaced) {
-				console.log("Connection Replaced, Another New Session Opened, Please Close Current Session First");
-				startHBWABotMz()
-			} else if (reason === DisconnectReason.loggedOut) {
-				console.log(`Device Logged Out, Please Delete Session and Scan Again.`);
-				startHBWABotMz();
-			} else if (reason === DisconnectReason.restartRequired) {
-				console.log("Restart Required, Restarting...");
-				startHBWABotMz();
-			} else if (reason === DisconnectReason.timedOut) {
-				console.log("Connection TimedOut, Reconnecting...");
-				startHBWABotMz();
-			} else HBWABotMz.end(`Unknown DisconnectReason: ${reason}|${connection}`)
-		}
 		if (update.connection == "connecting" || update.receivedPendingNotifications == "false") {
 			console.log(color(`\n🌿Connecting...`, 'yellow'))
 		}
