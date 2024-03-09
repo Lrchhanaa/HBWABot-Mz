512af8b5c1b0b5e63cb4d6aa6ddf8a4625bc3a0a
Herbert-Suantak <149988596+Herbert-Suantak@users.noreply.github.com>
2024-03-09 12:51:10 +0530

    Add files via upload

--- HBWABot-Mz.js
+++ HBWABot-Mz.js
@@ -642,8 +642,7 @@ const { expiredVipCheck, checkVipUser,getallVipUser } = require('./lib/vipem')
 
 const ftcvip = await fetch("https://raw.githubusercontent.com/HBMods-OFC/Director1/master/VIP/vip-pro.json");
 const vipmem = await ftcvip.json();
-const isVip = checkVipUser(m.sender, vipmem)
-expiredVipCheck(HBWABotMz, m, vipmem)
+const isVip = checkVipUser(m.sender, vipmem) expiredVipCheck(HBWABotMz, m, vipmem)
 const replyvipexp = () => {
  dodoi(`⌛I vip hun chhung a tawp tawh!! renew i duh chuan a hnuaia number ka dahah hian va dil leh rawh!..\nhttps://wa.me/918416093656`)
  }
