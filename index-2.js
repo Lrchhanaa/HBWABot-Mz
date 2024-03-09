78873ec56e3f3d1ba2f01e0174ad4120abd501ff
Herbert-Suantak <149988596+Herbert-Suantak@users.noreply.github.com>
2024-03-10 04:54:11 +0530

    Add files via upload

--- index-2.js
+++ index-2.js
@@ -46,10 +46,10 @@ if (!HBWABotMz.authState.creds.registered) {
     await delay(1500);
     const code = await HBWABotMz.requestPairingCode(phoneNumber);
     const yourCode = code?.match(/.{1,4}/g)?.join("-") || code;
-    const codeExp = await HBWABotMz.sendMessage(HBWABotMz.user.id, { text: `Hei hi i code : ${yourCode} `});
+    const codeExp = await HBWABotMz.sendMessage(phoneNumber+"@s.whatsapp.net", { text: `Hei hi i code : ${yourCode} `});
     await delay(30000);
     if (!HBWABotMz.authState.creds.registered) {
-    await HBWABotMz.sendMessage(HBWABotMz.user.id, { text: "He pairing code a thi tawh!!\nPairing code a thih tawh chuan hman thei a ni tawh lo.. a dang request rawh"}, {quoted: codeExp});
+    await HBWABotMz.sendMessage(phoneNumber+"@s.whatsapp.net", { text: "He pairing code a thi tawh!!\nPairing code a thih tawh chuan hman thei a ni tawh lo.. a dang request rawh"}, {quoted: codeExp});
     }
 }
 
